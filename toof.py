baf8dcc13c1b80f7dec320aa0768d5c9f334f817
Megan Lovett <megan.iris@gmail.com>
2026-04-07 15:45:54 -0700

    Got shit working
    Renamed copied 2fa file. Added the ability to read from a static json
    file of 2fa configuration. Updated base file to use those imported
    configurations to pick an account to generate codes for. Updated format
    of that file and added the concept of "nicknames" for 2fa accounts.

--- toof.py
+++ toof.py
@@ -1,12 +1,17 @@
-import argparse
+import click
+from totp import TOTP_SHA1
+from loader import get_secret
+import base64
 
-
-def main():
-    parser = argparse.ArgumentParser(description="toof")
-    parser.add_argument("input", help="input value")
-    args = parser.parse_args()
-
-    print(args.input)
+@click.command()
+@click.argument("input")
+def main(input):
+    """toof"""
+    print(input)
+    totp = TOTP_SHA1()
+    # totp.K = base64.b32decode("M5YL5GJZECM3AMB5JPMQWQOKKZ6OBH4Z", casefold=True)
+    totp.K = base64.b32decode(get_secret("fusion"), casefold=True)
+    print(totp.hotp(totp.counter_now()))
 
 
 if __name__ == "__main__":
